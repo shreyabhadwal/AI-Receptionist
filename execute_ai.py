7b0f9c5cb4b2ad41515e0c83252adf188264a97f
Shreya Bhadwal <shreya.bhadwal@gmail.com>
2023-05-20 23:53:10 +0530

    Initial Commits

---
+++ execute_ai.py
@@ -0,0 +1,56 @@
+from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser
+from langchain.prompts import StringPromptTemplate
+from langchain import OpenAI, SerpAPIWrapper, LLMChain
+from typing import List, Union
+from langchain.schema import AgentAction, AgentFinish
+from langchain.tools.file_management import WriteFileTool
+from langchain.tools.file_management.read import ReadFileTool
+import openai
+import re
+from tempfile import TemporaryDirectory
+from langchain.utilities.zapier import ZapierNLAWrapper
+from langchain.agents.agent_toolkits import ZapierToolkit
+from langchain.agents import initialize_agent
+import os
+from langchain.agents import AgentType
+from langchain import PromptTemplate
+
+os.environ["ZAPIER_NLA_API_KEY"] = os.environ.get("ZAPIER_NLA_API_KEY", "sk-ak-zAJqR1Fs5vsKN7pWhaFQit7Kug")
+openai.api_key = "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"
+
+def call_agent():
+    
+    openai.api_key = "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"
+
+    zapier = ZapierNLAWrapper()
+    toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
+
+    llm = OpenAI(temperature=0)
+    agent = initialize_agent(toolkit.get_tools(), llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+
+    return agent
+
+def answer_the_call():
+
+    template = """
+    You are an AI receptionist to a paper company, receiving calls from various people. 
+    Your fellow AI recptionist has executed the follwing tasks. 
+    Tell this informtion to the caller in a friendly manner.
+    Here are some examples:
+    ======
+    INFO: The message "Paul, you missed yesterday's lunch" has been successfully appended to the Messages document
+    ANSWER:Your message has been successfully noted. Is there anything else I can help you?
+    
+    INFO: The calendar invite has been successfully created.
+    ANSWER: Your meeting has been successfully scheduled. Is there anything else I can help you?
+    ========
+    {INFO}
+    =====
+    ANSWER: 
+    """
+    openai.api_key = "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"
+    llm = OpenAI(temperature=0)
+    receptionist_prompt = PromptTemplate(template=template, input_variables=["INFO"])
+    llm_answer_chain = LLMChain(llm=llm, prompt=receptionist_prompt)
+
+    return llm_answer_chain
---
+++ main.py
@@ -0,0 +1,58 @@
+#AI Receptionist 
+# Take down messages, maybe check calendar invites, provide information
+# Schedule a meeting/appointment, google calendar, ask for information, is the doctor available
+
+import sounddevice as sd
+import soundfile as sf
+import openai
+import keyboard
+import tempfile
+import os
+from elevenlabs import generate, play, set_api_key
+from execute_ai import call_agent, answer_the_call
+
+duration = 10  # duration of each recording in seconds
+fs = 44100  # sample rate
+channels = 1  # number of channels
+sd.default.samplerate = fs
+sd.default.channels = 2
+
+os.environ["OPENAI_API_KEY"] = "" #OpenAI Key
+set_api_key("")
+
+def record_audio(duration, fs, channels):
+    print("Recording...")
+    recording = sd.rec(int(duration * fs), samplerate=fs, channels=channels)
+    sd.wait()
+    print("Finished recording.")
+    return recording
+
+def transcribe_audio(recording, fs):
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_audio:
+        sf.write(temp_audio.name, recording, fs)
+        temp_audio.close()
+        with open(temp_audio.name, "rb") as audio_file:
+            transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        os.remove(temp_audio.name)
+    return transcript["text"].strip()
+
+def play_generated_audio(text, voice="Bella", model="eleven_monolingual_v1"):
+    audio = generate(text=text, voice=voice, model=model)
+    play(audio)
+
+if __name__ == '__main__':
+
+    inital_text = "Hi! You have reached Dunder Mifflin's office, this is Pam AI. What can I do for you today?"
+    play_generated_audio(inital_text)
+
+    while True:
+        print("Press spacebar to start recording.")
+        keyboard.wait("space")  # wait for spacebar to be pressed
+        recorded_audio = record_audio(duration, fs, channels)
+        message = transcribe_audio(recorded_audio, fs)
+        print(f"You: {message}")
+        agent = call_agent()
+        task_info = agent.run(message)
+        answer_chain = answer_the_call()
+        answer = answer_chain.predict(INFO = task_info)
+        play_generated_audio(answer) 
